bf511ac239aabed140ffd0b08ae89b0643033e03
igorekch1 <33809375+igorekch1@users.noreply.github.com>
2018-10-03 10:40:05 +0300

    hw4 added

---
+++ hw4/task1.js
@@ -0,0 +1,19 @@
+var handbag = {
+	size: "medium",
+	color: "red",
+	price: 1200,
+	content: ["духи","помада","сигареты","расческа","зеркальце","тушь","телефон","кошелёк"],
+	addItem: function(item){
+		this.content.push(item);
+	},
+	delItem: function(num){
+		this.content.splice(num,1);
+	}
+}
+console.log(handbag.content);
+console.log("--------------");
+handbag.addItem("блокнот");
+console.log(handbag.content);
+console.log("--------------");
+handbag.delItem(5);
+console.log(handbag.content);
---
+++ hw4/task2.js
@@ -0,0 +1,50 @@
+var LibraryBook = function (_title,_year,_author) {
+	var title = _title,
+		year = _year,
+	 	author = _author,
+	 	readerName =  null,
+	 	readerData = null;
+
+	giveTheBook = (function(client){
+		readerName = client;
+		readerData = new Date().toLocaleString().split(', ')[0];
+	})
+
+	this.getBookInfo = function(){
+		if (readerName) {
+			console.info(`Current owner of the book: ${readerName} \nWas taken on: ${readerData}` )}
+			 else {
+			 	console.info(`Title: "${_title}", Year: ${_year}, Author: ${_author} \nThe book is available!`);
+		}
+	}
+
+	this.getTheBook = function(client){
+		if (readerName){
+			this.getBookInfo();
+			return null;
+		} else{
+			giveTheBook(client);
+			return `"${title}", ${year}, ${author} was given to ${readerName}`;
+		}
+	}
+
+	this.returnBook = function(){
+		if (!readerName) console.info("This book has been already returned!") 
+			else{
+				readerData = null;
+				readerName = null;
+				console.log("Book is returned!");	
+			}
+	}
+}
+
+var books = [];
+books[0] = new LibraryBook("The picture of Dorian Gray",1890,"Oscar Wilde");
+
+console.log(books[0].getBookInfo());
+console.log(books[0].returnBook());
+console.log(books[0].getBookInfo());
+console.log(books[0].getTheBook("Vasiliy"));
+console.log(books[0].getBookInfo());
+console.log(books[0].returnBook());
+console.log(books[0].getBookInfo());
