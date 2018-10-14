02f5dd0542e593d606a3d0c35e5bd18677bc5f7a
igorekch1 <33809375+igorekch1@users.noreply.github.com>
2018-10-14 21:57:27 +0300

    add hw6 task2

---
+++ hw6/task2/script.js
@@ -0,0 +1,43 @@
+var tagNames = [ "div", "div", "div", "div", "button" ]
+var divStyle = `
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: solid 1px green;
+    font-size: 25px;
+`
+elements = tagNames.map ( function ( x ) {
+    return document.body.appendChild ( 
+        document.createElement ( x ) 
+    )
+})
+
+elements.filter ( function ( element ) {
+    return element.tagName === "DIV"
+})
+.forEach ( function ( element, num ) {
+            element.style = divStyle
+            element.innerText = num
+})
+
+elements.filter ( function ( element ) {
+    return element.tagName === "BUTTON"
+})
+.forEach ( function ( element ) {
+    element.innerHTML = "remove DIVs"
+    element.onclick = function ( event ) {
+        recursRemove()
+    }
+})
+
+var recursRemove = (function(tag){
+    var removeElem = document.body.querySelectorAll(tag);
+    var ind = 0;
+    return function removeDiv(){
+        while (removeElem.length !== ind ){
+            removeElem[ind].parentNode.removeChild(removeElem[ind++]);
+            removeDiv(); 
+        }
+    }
+})("DIV")
