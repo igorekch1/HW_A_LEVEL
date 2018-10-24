89aa2772d76990f129e8de35d4de13da7b9f5ac0
igorekch1 <33809375+igorekch1@users.noreply.github.com>
2018-10-24 17:02:11 +0300

    mod hw9

--- hw9/task2/script.js
+++ hw9/task2/script.js
@@ -1,4 +1,6 @@
-fetch("https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg")
+var request = new Request("https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg");
+
+fetch(request)
 	.then(response => {
 		response.blob().then(response =>{
 			url = URL.createObjectURL(response);
